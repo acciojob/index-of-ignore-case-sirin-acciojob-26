44ca60b221dc7870fa3c6314ab3fbe343cc17eb8
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-07-17 22:21:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str, subStr) {
+  // Convert both strings to lowercase to perform case-insensitive comparison
+  const lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
+  
+  // Use the indexOf method to find the first occurrence
+  return lowerStr.indexOf(lowerSubStr);
 }
+
+// Examples
+console.log(indexOfIgnoreCase("Hello World", "world"));  // 6
+console.log(indexOfIgnoreCase("apple", "Ple"));          // 2
+console.log(indexOfIgnoreCase("test", "aaa"));           // -1
+
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
